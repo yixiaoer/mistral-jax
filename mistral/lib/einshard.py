9c32f2c2fe9fb18fbff22380fdd0ea029c713983
yixiaoer <miyaku@yixiaoer.sg>
2024-03-08 20:35:32 +0800

    Debug einshard on v4-32
    * debug einshard for v4-32
    
    * einshard debug
    
    * einshard debug for v4-32
    
    * einshard debug for v4-32
    
    * einshard debug for v4-32
    
    * einshard debug for v4-32
    
    * debug for v4-32
    
    * debug for v4-32
    
    * debug for v4-32
    
    * debug for v4-32
    
    * for debug on v4-32
    
    * debug v4-32

--- mistral/lib/einshard.py
+++ mistral/lib/einshard.py
@@ -205,18 +205,38 @@ def einshard(arr: Array, expression: str) -> Array:
     d = {identifier: i for i, (identifier, _) in enumerate(elements_right) if identifier is not None}
     partition_spec = tuple(f'a{d[element_left]}' for element_left in elements_left)
 
+    # print('===========')
     # print(mesh_shape)
     # print(axis_names)
     # print(partition_spec)
 
+    # print('----------')
+
     devices = mesh_utils.create_device_mesh(mesh_shape)
     mesh = Mesh(devices, axis_names=axis_names)
-    arr = jax.device_put(arr, NamedSharding(mesh, P(*partition_spec)))
+    # print(mesh)
+    # arr = jax.device_put(arr, NamedSharding(mesh, P(*partition_spec)))
+    arr = jax.make_array_from_callback(arr.shape, NamedSharding(mesh, P(*partition_spec)), lambda idx: arr[idx])
     return arr
 
 # print(jax.device_count())
-# a = jnp.arange(32).reshape(8, 4)
+# a = jnp.arange(32*4).reshape(32, 4)
 # jax.debug.visualize_array_sharding(a)
 # a = einshard(a, '... -> 1 ...')
 # jax.debug.visualize_array_sharding(a)
-# a.device_buffers
+
+# b = jnp.arange(32*4).reshape(32, 4)
+# jax.debug.visualize_array_sharding(b)
+# b = einshard(b, 'x y -> x1 y')
+# jax.debug.visualize_array_sharding(b)
+
+# arr = jnp.arange(32*4).reshape(32, 4)
+# n_devices = jax.device_count() 
+# mesh_shape = [n_devices, 1]
+# axis_names = ('a1', 'a2')
+# partition_spec = ('a1', 'a2')
+# devices = mesh_utils.create_device_mesh(mesh_shape)
+# mesh = Mesh(devices, axis_names=axis_names)
+# arr = jax.make_array_from_callback(arr.shape, NamedSharding(mesh, P(*partition_spec)), lambda idx: arr[idx])
+# jax.debug.visualize_array_sharding(arr)
+# print(arr.addressable_data(0).shape)
--- train.py
+++ train.py
@@ -4,6 +4,7 @@ from functools import partial
 
 from jax import Array, value_and_grad
 import jax.numpy as jnp
+from jax_smi import initialise_tracking
 import optax
 from torch.utils.data import DataLoader
 from transformers import AutoTokenizer, MistralForCausalLM
@@ -14,6 +15,8 @@ from mistral.model.mistral_lm import MistralLMParams, forward_mistral_lm
 from mistral.model.rotary_embedding import RotaryValues, make_rotary_values
 from mistral.model.mistral_lm import convert_mistral_lm_params, shard_mistral_lm_params
 
+initialise_tracking()
+
 @value_and_grad
 def loss_and_grad(params: MistralLMParams, seq_ids: Array, qk_mask: Array, label_ids: Array, label_mask: Array, rotary_values: RotaryValues) -> Array:
     logits, _ = forward_mistral_lm(params, seq_ids, qk_mask, rotary_values, None)
@@ -23,13 +26,17 @@ def loss_and_grad(params: MistralLMParams, seq_ids: Array, qk_mask: Array, label
 
 def main():
     jax.distributed.initialize()
+    model = MistralForCausalLM.from_pretrained('mistralai/Mistral-7B-v0.1')
     tokenizer = AutoTokenizer.from_pretrained('mistralai/Mistral-7B-v0.1')
     tokenizer.pad_token = tokenizer.eos_token
 
-    # load on CPU first to avoid OOM
-    cpu_device = jax.devices('cpu')[0]
-    with jax.default_device(cpu_device):
-        model = MistralForCausalLM.from_pretrained('mistralai/Mistral-7B-v0.1')
+    if jax.local_device_count() == 8:
+        # if it's V3-8, load on CPU first to avoid OOM
+        cpu_device = jax.devices('cpu')[0]
+        with jax.default_device(cpu_device):
+            params = convert_mistral_lm_params(model)
+    elif jax.local_device_count() == 4:
+        # if it's V4-32
         params = convert_mistral_lm_params(model)
     params = shard_mistral_lm_params(params)
 

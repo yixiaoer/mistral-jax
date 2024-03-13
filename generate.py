Fix padding and precision

generate.py
@@ -5,6 +5,7 @@ import jax.random as jrand
 import torch
 from transformers import AutoTokenizer, MistralForCausalLM
 
+jax.config.update('jax_default_matmul_precision', jax.lax.Precision.HIGHEST)
 jax.config.update('jax_spmd_mode', 'allow_all')
 
 from mistral.model.mistral_lm import convert_mistral_lm_params, shard_mistral_lm_params
@@ -32,22 +33,22 @@ def main():
     key = jrand.key(42)
     key, subkey = jrand.split(key)
 
-    output_ids = generate(params, sentences, tokenizer, max_length, max_new_tokens)
+    output_ids = generate(params, tokenizer, sentences, max_length, max_new_tokens)
     output = tokenizer.batch_decode(output_ids, skip_special_tokens=True)
 
-    output_id_sampling = generate(params, sentences, tokenizer, max_length, max_new_tokens, key=subkey, top_k=5, top_p=0.8, temperature=0.9)
+    output_id_sampling = generate(params, tokenizer, sentences, max_length, max_new_tokens, key=subkey, top_k=5, top_p=0.8, temperature=0.9)
     output_sampling = tokenizer.batch_decode(output_id_sampling, skip_special_tokens=True)
 
     inputs_pt = tokenizer(sentences, padding='max_length', max_length=max_length, return_tensors='pt')
     with torch.no_grad():
-        generated_pt = model.generate(input_ids=inputs_pt.input_ids, attention_mask=inputs_pt.attention_mask, do_sample=False, max_new_tokens=max_new_tokens)
+        generated_pt = model.generate(input_ids=inputs_pt.input_ids, attention_mask=inputs_pt.attention_mask, do_sample=False, max_new_tokens=max_new_tokens, max_length=max_length)
     output_pt = tokenizer.batch_decode(generated_pt, skip_special_tokens=True)
 
     print(f'JAX(sampling) output: {output_sampling}')
     print(f'JAX output: {output}')
     print(f'PyTorch output: {output_pt}')
     print(f'JAX output == PyTorch output: {output == output_pt}')
-    # JAX(sampling) output: ["How have you been?\nWe're still working our dayj0bes, and I think it has taken me until today for it finally hit us: We don`T work", 'The Lord of the Rings is a trilogy of high fantasy novels written by English author J. R. R. Tolkien. The trilogy, set in the fictional world']
+    # JAX(sampling) output: ["How have you been?\nWe're still working our dayj0bes, and I think it has taken me until today for it finally feel that the year is starting to roll", 'The Lord of the Rings is a trilogy of high fantasy novels written by English author J. R. R. Tolkien. The trilogy, collectively titled The Lord of']
     # JAX output: ['How have you been? I’ve been busy with work and life, but I’m still here. I’m still here.\n\nI’ve been thinking about this', 'The Lord of the Rings is a series of three epic fantasy novels written by J. R. R. Tolkien. The books tell of the quest of a group of heroes to destroy a']
     # PyTorch output: ['How have you been? I’ve been busy with work and life, but I’m still here. I’m still here.\n\nI’ve been thinking about this', 'The Lord of the Rings is a series of three epic fantasy novels written by J. R. R. Tolkien. The books tell of the quest of a group of heroes to destroy a']
     # JAX output == PyTorch output: True

mistral/lib/generate.py
@@ -1,33 +1,31 @@
 from typing import Callable
 
-from jax import Array, jit, lax
+from jax import Array, lax
 from jax.nn import softmax
 import jax.numpy as jnp
 import jax.random as jrand
 from transformers import AutoTokenizer, MistralForCausalLM
 
-from ..model.kvcache import KVCache, left_shift_kv_cache
+from ..model.kvcache import KVCache
 from ..model.mistral_lm import MistralLMParams, forward_mistral_lm
-from ..model.rotary_embedding import get_rotary_values_at_position, make_rotary_values
+from ..model.rotary_embedding import RotaryValues, get_rotary_values_at_position, make_rotary_values
 
-def generate(params: MistralLMParams, sentences: list[str], tokenizer: AutoTokenizer, max_length: int, max_new_tokens: int, *, key: Array | None = None, top_k: int | None = None, top_p: float | None = None, temperature: float = 1.) -> Array:
-    inputs = tokenizer(sentences, padding='max_length', max_length=max_length, return_tensors='jax')
+def generate(params: MistralLMParams, tokenizer: AutoTokenizer, sentences: list[str], max_length: int, max_new_tokens: int, *, key: Array | None = None, top_k: int | None = None, top_p: float | None = None, temperature: float = 1.) -> Array:
+    # `max_length` and `max_new_tokens` jointly influence the maximum number of tokens generated
+    inputs = tokenizer(sentences, padding=True, return_tensors='jax')
     input_ids = inputs.input_ids.astype(jnp.uint16)
     output_ids = input_ids
-    batch_size = len(input_ids)
+    batch_size, batch_len = input_ids.shape
     attn_mask = inputs.attention_mask.astype(jnp.bool_)
     qk_mask = jnp.tril(jnp.einsum('bi,bj->bij', attn_mask, attn_mask))[:, None, None]
     kv_cache = None
 
-    padding_len = jnp.sum(attn_mask == True, axis=1)
-    num_generate_tokens = min(*(max_length - padding_len), max_new_tokens)
-
-    rotary_values_cur = make_rotary_values(padding_len, batch_size, max_length)
-    rotary_values = make_rotary_values(padding_len, batch_size, max_length)
-
-    for idx in range(num_generate_tokens):
+    generate_tokens_n = min(*(max_length - jnp.sum(attn_mask == True, axis=1)), max_new_tokens)
+    rotary_values_cur = make_rotary_values(batch_size, batch_len)
+    rotary_values = make_rotary_values(batch_size, batch_len + generate_tokens_n)
+    for idx in range(generate_tokens_n):
         if idx != 0:
-            position = jnp.array((idx - 1), jnp.int16)
+            position = jnp.array((batch_len + idx - 1), jnp.int16)
             rotary_values_cur = get_rotary_values_at_position(rotary_values, position)
         logits, kv_cache = forward_mistral_lm(params, input_ids, qk_mask, rotary_values_cur, kv_cache)
         logits = logits[:, -1]
@@ -44,14 +42,13 @@ def generate(params: MistralLMParams, sentences: list[str], tokenizer: AutoToken
             next_token = sampling(logits, tokens_ids, key)
 
         input_ids = next_token
-        output_ids = jnp.concatenate((output_ids[:,1:], input_ids), axis=1)
-        attn_mask = jnp.concatenate((attn_mask[:,1:], jnp.ones((batch_size, 1), dtype=bool)), axis=1)
+        output_ids = jnp.concatenate((output_ids, next_token), axis=1)
+        attn_mask = jnp.concatenate((attn_mask, jnp.ones((batch_size, 1), dtype=bool)), axis=1)
         qk_mask = attn_mask[:, None, None, None, :]
-        kv_cache = left_shift_kv_cache(kv_cache)
     return output_ids
 
 def greedy_search(logits: Array) -> Array:
-    return jnp.argmax(logits, axis=-1).reshape(-1,1)
+    return jnp.argmax(logits, axis=-1).reshape(-1, 1)
 
 def top_k_logits(logits: Array, *, temperature: float = 1.0, top_k: int) -> tuple[Array, Array]:
     top_k_logits, top_k_ids = lax.top_k(logits, top_k)  # along the last axis
@@ -62,12 +59,13 @@ def top_p_logits(logits: Array, *, tokens_ids: Array | None = None, temperature:
     if tokens_ids is None:
         tokens_ids = jnp.argsort(-logits, axis=-1)
         logits = jnp.take_along_axis(logits, tokens_ids, axis=-1)
+
     probs_sorted_cumsum = jnp.cumsum(softmax(logits, axis=-1), axis=-1)
-    cutoff_index = jnp.sum(probs_sorted_cumsum < top_p, axis=-1).reshape(-1,1)
+    cutoff_index = jnp.sum(probs_sorted_cumsum < top_p, axis=-1).reshape(-1, 1)
     logits = jnp.where(jnp.arange(logits.shape[-1])[None, :] <= cutoff_index, logits, -jnp.inf)
     return logits / temperature, tokens_ids
 
 def sampling(sampling_logits: Array, tokens_ids: Array, key: Array | None = None) -> Array:
-    sampling_index = jrand.categorical(key, softmax(sampling_logits)).reshape(-1,1)
+    sampling_index = jrand.categorical(key, softmax(sampling_logits)).reshape(-1, 1)
     selected_token = jnp.take_along_axis(tokens_ids, sampling_index, axis=-1)
     return selected_token

mistral/model/attention.py
@@ -43,8 +43,8 @@ def shard_attention_params(params: AttentionParams) -> AttentionParams:
     # k_proj = einshard(k_proj, 'm h k -> m h1 k')
     # v_proj = einshard(v_proj, 'm h v -> m h1 v')
     # o_proj = einshard(o_proj, 'r h v m -> r h1 v m')
-    # q_proj = einshard(q_proj, 'm r h k -> m r h k1')
-    # k_proj = einshard(k_proj, 'm h k -> m h k1')
+    q_proj = einshard(q_proj, 'm r h k -> m r h k1')
+    k_proj = einshard(k_proj, 'm h k -> m h k1')
     v_proj = einshard(v_proj, 'm h v -> m h v1')
     o_proj = einshard(o_proj, 'r h v m -> r h v1 m')
     return q_proj, k_proj, v_proj, o_proj
@@ -74,8 +74,8 @@ def forward_attention(params: AttentionParams, seq: Array, qk_mask: Array, rotar
     if kv_cache_cur is None:
         kv_cache_cur = [], []
     if kv_cache_pre is not None:
-        k = jnp.concatenate((kv_cache_pre[0].pop(0)[:,:,:-1,:], k), axis=2)
-        v = jnp.concatenate((kv_cache_pre[1].pop(0)[:,:,:-1,:], v), axis=2)
+        k = jnp.concatenate((kv_cache_pre[0].pop(0), k), axis=2)
+        v = jnp.concatenate((kv_cache_pre[1].pop(0), v), axis=2)
 
     kv_cache_cur[0].append(k)
     kv_cache_cur[1].append(v)
@@ -106,7 +106,7 @@ def test_forward_attention(model: MistralForCausalLM) -> None:
     seq_jax = pt2jax(seq_pt)
     attention_mask_jax = pt2jax(attention_mask_pt)
     batch_size, seq_len, _ = seq_jax.shape
-    rotary_values = make_rotary_values(None, batch_size, seq_len)
+    rotary_values = make_rotary_values(batch_size, seq_len)
     out_jax, _, _ = forward_attention(params, seq_jax, attention_mask_jax, rotary_values, None, None)
 
     assert jnp.allclose(out_jax, pt2jax(out_pt), atol=1e-5)

mistral/model/mistral_lm.py
@@ -55,7 +55,7 @@ def test_forward_mistral_lm(model: MistralForCausalLM) -> None:
     qk_mask = jnp.tril(jnp.einsum('bi,bj->bij', attn_mask_jax, attn_mask_jax))[:, None, None]
 
     batch_size, seq_len = input_ids_jax.shape
-    rotary_values = make_rotary_values(None, batch_size, seq_len)
+    rotary_values = make_rotary_values(batch_size, seq_len)
 
     outputs_jax, _ = forward_mistral_lm(params, input_ids_jax, qk_mask, rotary_values, None)
 

mistral/model/rotary_embedding.py
@@ -39,23 +39,17 @@ def forward_rotary_embedding(m: Array, *, rotary_values: RotaryValues) -> Array:
     b = op.einsum(n, sin_val, 'B ... L K, B L K -> B ... L K').astype(m.dtype)
     return a + b
 
-def make_rotary_values(padding_len: Array | None, batch_size: int, seq_len: int) -> RotaryValues:
+def make_rotary_values(batch_size: int, seq_len: int) -> RotaryValues:
     sin_val, cos_val = _make_weights(seq_len, d_k)
 
     sin_val = jnp.repeat(sin_val[None], batch_size, axis=0)
     cos_val = jnp.repeat(cos_val[None], batch_size, axis=0)
-
-    if padding_len is not None:
-        roll_func = jax.vmap(lambda a, shift: jnp.roll(a, shift, axis=-2))  # -2: dimension L
-        sin_val = roll_func(sin_val, - padding_len)
-        cos_val = roll_func(cos_val, - padding_len)
-
     return RotaryValues(sin_val, cos_val)
 
 def get_rotary_values_at_position(rotary_values: RotaryValues, position: Array) -> RotaryValues:
     sin_val, cos_val = rotary_values
     sin_val = sin_val[:, position][:, None]
     cos_val = cos_val[:, position][:, None]
-    
+
     rotary_values = RotaryValues(sin_val, cos_val)
     return rotary_values